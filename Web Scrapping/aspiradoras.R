48f396d17ef41fe3578ccba5f389052b025c5ea5
OscarCanongo <oscarcanongo@outlook.com>
2020-12-22 18:18:02 -0600

    Obteniendo el URL de cada producto

--- Web Scrapping/aspiradoras.R
+++ Web Scrapping/aspiradoras.R
@@ -11,8 +11,35 @@ urlcompleta <- paste0("www.amazon.es", nodo_links)
 urlcompleta
 
 library(stringr)
-pag <- "https://www.amazon.es/s?k=Aspiradoras&page=2&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1608589177&ref=sr_pg_2"
+pag <- "s?k=Aspiradoras&page=2&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1608589177&ref=sr_pg_2"
 lista_paginas <- c(1:10)
 pag <- str_replace(pag, "page=2", paste0("page=", lista_paginas))
 pag <- str_replace(pag, "sr_pg_2", paste0("sr_pg_", lista_paginas))
 pag
+
+paginas <- paste0("https://www.amazon.es/", pag)
+paginas
+
+dameLinksPagina <- function(url){
+  selector<-"div > span > div > div > div.a-section.a-spacing-none.a-spacing-top-small > h2 > a"
+  pagina<-read_html(url)
+  nodo<-html_nodes(pagina,selector)
+  nodo_text<-html_text(nodo)
+  nodo_links<-html_attr(nodo,"href")
+  nodo_links  
+}
+
+paginas[3]
+
+test <- dameLinksPagina(paginas[3])
+test
+
+linksAsp <- sapply(paginas, dameLinksPagina)
+class(linksAsp)
+dim(linksAsp)
+
+vlink <- as.vector(linksAsp)
+length(vlink)
+
+vlinkAspiradora <- paste0("https://www.amazon.es/", vlink)
+vlinkAspiradora
